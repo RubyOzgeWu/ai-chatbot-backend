96ffd2292d15251819db2b9e61ac8e8311b4437e
RubyOzgeWu <rubyozgewu@gmail.com>
2025-04-07 16:40:55 +0800

    fix: 調整端點路徑
    - 暫時移除 user 路徑功能

--- src/routes/conversations.js
+++ src/routes/conversations.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import content from '../middleware/content.js'
-import * as auth from '../middleware/auth.js'
+import express from "express";
+import content from "../middleware/content.js";
+import * as auth from "../middleware/auth.js";
 
 import {
   createConversation,
   readConversations,
   readConversation,
   deleteConversations,
-} from '../controllers/users.js'
+} from "../controllers/conversation.js";
 
+const router = express.Router();
 
-const router = express.Router()
-
-router.post('/', content('application/json'), createConversation)
-router.get('/user/:user_id', auth.jwt, readConversations);
-router.get('/:session_id', auth.jwt, readConversation);
-router.delete('/user/:user_id', auth.jwt, deleteConversations);
+router.post("/", content("application/json"), createConversation);
+router.get("/all", readConversations); // 取得所有對話
+router.get("/:session_id", readConversation); // 取得單一對話
+router.delete("/all", deleteConversations);
+// router.get('/user/:user_id', auth.jwt, readConversations);
+// router.delete('/user/:user_id', auth.jwt, deleteConversations);
+export default router;
